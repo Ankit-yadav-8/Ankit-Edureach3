ceac35d92028c06caf1c0e7f8a294041e891be3f
Ankit-IITR <ankityadav08022008@gmail.com>
2026-05-28 13:26:59 +0530

    rebrand: EduReach to CollegeParichay

--- src/workers/collegePredictor.worker.js
+++ src/workers/collegePredictor.worker.js
@@ -1,13 +1,20 @@
-import { predictColleges, predictCollegesGrouped } from "../utils/collegePredictor.js";
 
-self.onmessage = ({ data }) => {
+import { predictColleges, predictCollegesGrouped } from "../utils/collegePredictor.js";
+import { loadCutoffDB } from "../utils/realCutoffEngine.js";
+
+self.onmessage = async ({ data }) => {
   try {
     const { type, opts } = data;
+
+    // Ensure JoSAA CSVs are loaded *in this worker's context* first.
+    await loadCutoffDB();
+
     const result = type === "grouped"
       ? predictCollegesGrouped(opts)
       : predictColleges(opts);
+
     self.postMessage({ ok: true, result });
   } catch (err) {
-    self.postMessage({ ok: false, error: err.message });
+    self.postMessage({ ok: false, error: err?.message || "Prediction failed" });
   }
 };
